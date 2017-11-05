8f9bcae3c5cddb3ed6c1fb22986f9c19a81abdfb
Armandas Dambrauskas <armas_z@yahoo.com>
2017-11-05 17:41:34 +0200

    Adding project

---
+++ js/main.js
@@ -0,0 +1,14 @@
+function check_info() {
+  var fname = document.getElementById('fname').value;
+  var lname = document.getElementById('lname').value;
+  var msq = document.getElementById('msg').value;
+
+  if(fname=="" || lname=="" || msg=="") {
+    alert('Please fill in all fields');
+    return false;
+  }
+  else{
+    return true;
+
+  }
+}
