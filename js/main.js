2304b7fae293a865189e55a52deffedd129af8ec
Armandas Dambrauskas <armas_z@yahoo.com>
2017-11-07 16:45:02 +0200

    edit

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 function check_info() {
   var fname = document.getElementById('fname').value;
   var lname = document.getElementById('lname').value;
-  var msq = document.getElementById('msg').value;
+  var msq = document.getElementById('textarea').value;
 
   if(fname=="" || lname=="" || msg=="") {
     alert('Please fill in all fields');
